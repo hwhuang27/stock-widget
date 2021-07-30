f4b69c50bba5e57ffe8a028421cd665ab5ffcd07
hwhuang27 <hwhuang@sfu.ca>
2021-07-30 12:28:00 -0700

    notification system WIP

--- src/stonks.py
+++ src/stonks.py
@@ -5,10 +5,12 @@ import pandas as pd
 import yfinance as yf
 from datetime import datetime
 
+# get stock data from yahoo finance
 def get_ticker_data(symbol):
     return yf.download(tickers=symbol, period="1d", interval="1m")
 
-def generate_open_price(symbol):
+# generate market price every minute
+def generate_open_price(symbol, notify_price):
     starttime = time.time()
     while True:
         now = datetime.now()
@@ -16,18 +18,31 @@ def generate_open_price(symbol):
         data = get_ticker_data(symbol)
         current_open = data.iloc[[-1]]['Open']
 
+        open_price = round(current_open[0], 2)
         print(symbol + " @ " + current_time)
-        print(round(current_open[0], 2))
+        print(open_price)
+        print(open_price > notify_price) # true
+        print(open_price < notify_price) # false
+        print(open_price = notify_price) # false
+
+        if open_price <= notify_price:
+            # notification code
+            print("BUY NOW!!!")
+
         time.sleep(60.0 - ((time.time() - starttime) % 60.0))
 
+# set notification when price goes below threshold
 def notify():
+
+
     return
 
 def main():
     #symbol = input("Enter ticker name: ")
     symbol = "VEQT.TO"
-    generate_open_price(symbol)
+    #min_open = input("Enter min. price for notification: ")
+    min_open = 35.15
+    generate_open_price(symbol, min_open)
 
 if __name__ == '__main__':
     main()
-
