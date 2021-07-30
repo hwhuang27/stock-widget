5692c990f97a0948a2040977521bf762c754751a
hwhuang27 <hwhuang@sfu.ca>
2021-07-30 12:17:38 -0700

    clean up display

--- src/stonks.py
+++ src/stonks.py
@@ -1,8 +1,9 @@
 import sys
+import time
 import numpy as np
 import pandas as pd
 import yfinance as yf
-import time
+from datetime import datetime
 
 def get_ticker_data(symbol):
     return yf.download(tickers=symbol, period="1d", interval="1m")
@@ -10,10 +11,17 @@ def get_ticker_data(symbol):
 def generate_open_price(symbol):
     starttime = time.time()
     while True:
+        now = datetime.now()
+        current_time = now.strftime("%H:%M")
         data = get_ticker_data(symbol)
         current_open = data.iloc[[-1]]['Open']
-        print(current_open)
+
+        print(symbol + " @ " + current_time)
+        print(round(current_open[0], 2))
         time.sleep(60.0 - ((time.time() - starttime) % 60.0))
+
+def notify():
+    return
 
 def main():
     #symbol = input("Enter ticker name: ")
