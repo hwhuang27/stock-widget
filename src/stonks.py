c5b018921e899164096a6891413d54d484df97b1
hwhuang27 <hwhuang@sfu.ca>
2021-07-30 12:52:54 -0700

    testing notifications

--- src/stonks.py
+++ src/stonks.py
@@ -3,7 +3,8 @@ import time
 import numpy as np
 import pandas as pd
 import yfinance as yf
-from datetime import datetime
+import subprocess as sp
+from datetime import datetime, date
 
 # get stock data from yahoo finance
 def get_ticker_data(symbol):
@@ -15,26 +16,24 @@ def generate_open_price(symbol, notify_price):
     while True:
         now = datetime.now()
         current_time = now.strftime("%H:%M")
+        today = date.today()
+        current_date = today.strftime("%b-%d-%Y")
         data = get_ticker_data(symbol)
         current_open = data.iloc[[-1]]['Open']
 
         open_price = round(current_open[0], 2)
-        print(symbol + " @ " + current_time)
+        print(symbol + " @ " + current_time + " (" + current_date + ")")
         print(open_price)
-
-        if (open_price <= notify_price):
+        print("\n")
+        if (open_price > notify_price):
+            # change the signs when done testing
             notify()
-
 
         time.sleep(60.0 - ((time.time() - starttime) % 60.0))
 
 # set notification when price goes below threshold
 def notify():
-    # notification code here
-    
-
-
-    print("BUY NOW!!!") # placeholder
+    sp.call(["notify-send", "Title", "some text"])
     return
 
 def main():
